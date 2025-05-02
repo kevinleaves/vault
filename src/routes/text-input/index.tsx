278e0e47053fd0c590ebc88c928e93cb2d8e41fa
Kevin Le <iamkevinvle@gmail.com>
2025-05-02 16:29:35 -0700

    text-input bg to white

--- src/routes/text-input/index.tsx
+++ src/routes/text-input/index.tsx
@@ -7,49 +7,51 @@ export const Route = createFileRoute('/text-input/')({
 
 function RouteComponent() {
   return (
-    <div className="pt-[112px] w-[340px] mx-auto flex flex-col gap-12">
-      <Input
-        labelText="Email"
-        disabled={false}
-        subText={'This is a hint text.'}
-        placeholder="name@email.com"
-        id="email"
-        iconLeading={<i className="ri-mail-line"></i>}
-        iconTrailing={<i className="ri-question-line"></i>}
-        type="email"
-        name="email"
-      />
-      <Input
-        labelText="Email"
-        disabled={false}
-        subText={'This is a hint text.'}
-        placeholder="name@email.com"
-        id="email"
-        iconTrailing={<i className="ri-question-line"></i>}
-        type="email"
-        name="email"
-      />
-      <Input
-        labelText="Email"
-        disabled={false}
-        subText={'This is a hint text.'}
-        placeholder="name@email.com"
-        id="email"
-        iconTrailing={<i className="ri-question-line"></i>}
-        type="email"
-        name="email"
-      />
-      <Input
-        labelText="Email"
-        disabled={false}
-        subText={'This is an error message'}
-        placeholder="name@email.com"
-        error={true}
-        id="email"
-        iconTrailing={<i className="ri-question-line"></i>}
-        type="email"
-        name="email"
-      />
+    <div className="pt-[112px] w-full bg-white min-h-screen">
+      <div className="flex flex-col gap-12 w-[340px] mx-auto">
+        <Input
+          labelText="Email"
+          disabled={false}
+          subText={'This is a hint text.'}
+          placeholder="name@email.com"
+          id="email"
+          iconLeading={<i className="ri-mail-line"></i>}
+          iconTrailing={<i className="ri-question-line"></i>}
+          type="email"
+          name="email"
+        />
+        <Input
+          labelText="Email"
+          disabled={false}
+          subText={'This is a hint text.'}
+          placeholder="name@email.com"
+          id="email"
+          iconTrailing={<i className="ri-question-line"></i>}
+          type="email"
+          name="email"
+        />
+        <Input
+          labelText="Email"
+          disabled={false}
+          subText={'This is a hint text.'}
+          placeholder="name@email.com"
+          id="email"
+          iconTrailing={<i className="ri-question-line"></i>}
+          type="email"
+          name="email"
+        />
+        <Input
+          labelText="Email"
+          disabled={false}
+          subText={'This is an error message'}
+          placeholder="name@email.com"
+          error={true}
+          id="email"
+          iconTrailing={<i className="ri-question-line"></i>}
+          type="email"
+          name="email"
+        />
+      </div>
     </div>
   )
 }
